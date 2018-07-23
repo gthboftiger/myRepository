ba71381855c6637633d8f2c89c83e544cf8d77ed
tigerkey <tiger@192.168.126.1>
2018-07-23 15:23:43 +0800

    commit

--- gitTest/src/main/java/com/tiger/GitTets.java
+++ gitTest/src/main/java/com/tiger/GitTets.java
@@ -4,7 +4,7 @@ public class GitTets {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello ! My first Github project!");
+		System.out.println("Hello ! My first Github project!");
 	}
 
 }
